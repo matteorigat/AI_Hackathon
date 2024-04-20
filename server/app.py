2bf941c25c93689353af142b5865ad439418e42d
Giacomo <gmonta2000@gmail.com>
2024-04-20 20:51:17 +0200

    fixed server

--- server/app.py
+++ server/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, send_file, request
 import os
-import shutil
 
 app = Flask(__name__)
 
@@ -11,10 +10,23 @@ def test():
 @app.route('/send', methods=['POST'])
 def send():
     if request.method == 'POST':
-        data = request.data
-        print(data)
+        json = request.json
+        bytes_base64 = json.get('data')
+        filename = json.get('fileName')
+        
+        folder_name = "data"
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+            print("Folder created correctly")
 
+        print(bytes_base64['data'])
+        rebuild_file(bytes(bytes_base64['data']), filename)
         return 'File salvato con successo', 200
+    
+def rebuild_file(bytes, filename):    
+    with open("data/" + filename, 'wb') as file:
+        file.write(bytes)    
+        print("File successfully rebuilt.")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
