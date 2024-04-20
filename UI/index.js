188c1e79267a411c12fd592b7ae53ecea5a1b719
Giacomo <gmonta2000@gmail.com>
2024-04-20 12:39:41 +0200

    created ui skeleton

---
+++ UI/index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('index.html');
+
+    // Uncomment the following line if you want to open the DevTools automatically
+    // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
